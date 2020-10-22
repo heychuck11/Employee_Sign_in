5d05e898d4b9ada7f74b07bc7749a260b234fb78
Charlie Howard <chuckhoward99@gmail.com>
2020-10-22 09:45:19 -0600

    a few spelling errors fixed

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const Manager = require("../lib/Manager");
+const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee")
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,6 +9,141 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+
+let teamMembers= []
+let idArr = []
+
+function startQuest(){
+  function employeeType(){
+    console.log(teamMembers)
+    inquirer.prompt({
+      type: "list",
+      message: "What type of employee would you like to add?",
+      name: "empType",
+      choices: ["manager", "engineer", "intern", "done"]
+    })
+    .then(res => {
+      console.log(res)
+      switch(res.empType){
+        case "manager":
+          managerQuest()
+          break;
+          case "engineer":
+            engineerQuest()
+            break;
+            case "intern":
+              internQuest()
+              break;
+              case "done":
+              done()
+              break;
+      }
+    })
+  }
+  function managerQuest(){
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the manager's name?",
+        name: "managerName"
+      },
+      {
+        type: "input",
+        message: "what is your manager id number?",
+        name: "managerId"
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "managerEmail"
+
+      },
+      {
+        type: "input",
+        message:"What is your office number?",
+        name:"managerOffice"
+      }
+    ])
+    .then(res =>{
+      console.log(res)
+      const manager = new Manager(res.managerName, res.managerId, res.managerEmail, res.managerOffice)
+      teamMembers.push(manager)
+      idArr.push(res.managerId)
+      employeeType()
+    })
+  }
+  function engineerQuest(){
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "engineerName"
+      },
+      {
+        type: "input",
+        message: "what is your engineer id number?",
+        name: "engineerId"
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "engineerEmail"
+
+      },
+      {
+        type: "input",
+        message:"What is your github username?",
+        name:"engineerUserName"
+      }
+    ])
+    .then(res =>{
+      console.log(res)
+      const engineer = new Engineer(res.engineerName, res.engineerId, res.engineerEmail, res.engineerUserName)
+      teamMembers.push(engineer)
+      idArr.push(res.engineerId)
+      employeeType()
+    })
+  }
+  function internQuest(){
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the intern's name?",
+        name: "internName"
+      },
+      {
+        type: "input",
+        message: "what is your intern id number?",
+        name: "internId"
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "internEmail"
+
+      },
+      {
+        type: "input",
+        message:"What is the name of your school?",
+        name:"internSchool"
+      }
+    ])
+    .then(res =>{
+      console.log(res)
+    })
+  }
+  function done(){
+    if(!fs.existsSync(OUTPUT_DIR)){
+      fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8")
+  }
+  employeeType()
+
+}
+startQuest()
+
 
 
 
